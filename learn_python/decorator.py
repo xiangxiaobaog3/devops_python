87f9a72512c168d4bcd01fb094599d7512cad8a8
xiangxiaobaog3 <xiangxiaobaog3@gmail.com>
2020-04-06 21:09:40 +0800

    装饰器学习

---
+++ learn_python/decorator.py
@@ -0,0 +1,23 @@
+import time
+
+def timmer(f):
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        # 添加额外的功能：执行被装饰函数之前的操作
+        r = f(*args, **kwargs)
+        # 添加额外的功能：执行被装饰函数之前的操作
+        end_time = time.time()
+        print(f'测试执行效率{end_time-start_time}')
+        return r
+    return inner
+
+@timmer
+def index(name):
+    time.sleep(3)
+    print(f'2{name}')
+    return 666
+
+
+
+ret = index("xiang")
+print(ret)
